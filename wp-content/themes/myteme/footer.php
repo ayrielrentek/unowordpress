547e0899487a1423d8e2fd9222b20942aba91499
Ayriel Noriega Monroy <Ayriel Noriega@RENTEK.PRINCIPAL.COM>
2023-06-28 11:48:13 -0500

    add files template

---
+++ wp-content/themes/myteme/footer.php
@@ -0,0 +1,10 @@
+    <!-- FOOTER -->
+    <footer class="container">
+        <p class="float-end"><a href="#">Back to top</a></p>
+        <p>&copy; 2017–2023 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    </footer>
+    </main>
+    <script src="<?php echo get_template_directory_uri();?>/assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
